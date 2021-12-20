c34f2c763800e54261f56f0da5511aee12d54b7f
alok2110 <aloksaxena755@gmail.com>
2021-12-20 19:14:21 +0530

    change

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const cors = require("cors");
 
 require("dotenv").config();
 const app = express();
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(bodyParser.json());
 app.use("/", ContactRoutes);
